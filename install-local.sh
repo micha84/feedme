02c9e15967eaaddfdcdb6382d5117e0378068542
Michael Arndt <michael.arndt@exconcept.com>
2019-10-17 15:41:35 +0200

    initial project version

---
+++ install-local.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+ln -snf docker-sync.unison.yml docker-sync.yml
+ln -snf docker-compose.local.yml docker-compose.yml
+ln -snf .environment/.env.dist .env
+cd htdocs
+ln -snf .env.local .env
+cd app/config
+ln -snf config.local.php config.php
+cd ../../
